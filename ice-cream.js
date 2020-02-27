de7fd968dcc2e9700f6111d36da47913ada412a2
AndrewZapotochnyi <info@azblockchain.co>
2020-02-27 00:36:18 +0000

    Arrays of Ice Cream

---
+++ ice-cream.js
@@ -0,0 +1,18 @@
+let iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// Your code below here...
+
+//should be logging the original flavours along with root beer
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours);
+
+//should be logging the first flavour, ie chocolate
+console.log(iceCreamFlavours[0]);
+
+//should be logging the last flavour, ie root beer
+console.log(iceCreamFlavours[iceCreamFlavours.length - 1]);
+
+//should be logging the number of items in the array
+console.log(iceCreamFlavours.length);
+
